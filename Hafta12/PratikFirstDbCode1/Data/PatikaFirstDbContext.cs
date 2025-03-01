ed76331c1e2c068694b5c3ccf14a2b411578df5e
Burak Başol <117232704+bburakbbasol@users.noreply.github.com>
2025-03-01 16:44:59 +0300

    Add files via upload

---
+++ Hafta12/PratikFirstDbCode1/Data/PatikaFirstDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication;
+using PratikFirstDbContext.Entites;
+
+namespace PratikFirstDbContext.Data
+{
+	public class PatikaFirstDbContext:DbContext
+	{
+		public PatikaFirstDbContext(DbContextOptions<PatikaFirstDbContext> options) : base(options)
+		{
+
+		}
+		public DbSet<Game> Games { get; set; }
+		public DbSet<Movie> Movies { get; set; }
+
+	}
+}
---
+++ Hafta12/PratikFirstDbCode1/Entites/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace PratikFirstDbContext.Entites
+{
+	public class Game
+	{
+			public int Id { get; set; }
+			public string Name { get; set; }
+			public decimal Rating { get; set; }
+
+		
+	}
+}
